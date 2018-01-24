185eb1bb14ee82064613de90e3d1696abe32dd5f
Alexandra Havia <sasha.havia@gmail.com>
2018-01-23 20:12:32 -0500

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,12 +51,26 @@ module ApplicationHelper
       nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
-
-
     nav_links.html_safe
   end
 
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Sasha Havia Portfolio", sticky: false)
+  end
+
 end
+
+
+
+
