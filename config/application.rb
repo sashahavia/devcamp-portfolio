db727ce781b5800b0ddd1361b453314372758ed6
Alexandra Havia <sasha.havia@gmail.com>
2018-02-04 18:42:36 -0500

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module DevcampPortfolio
     config.generators.system_tests = nil
 
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     
   end
 end
