d01dfee099b06931de374fad2e7e1e9f794289d4
Alexandra Havia <sasha.havia@gmail.com>
2018-02-04 21:14:56 -0500

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://sashahavia-portfolio.herokuapp.com', 'http://sashahavia-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://sashahavia-portfolio.herokuapp.com/cable"
 end
